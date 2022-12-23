c6ae7f3b306ba4f2641f51c7519ada9f8bc8d4e1
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2022-12-23 12:03:44 -0300

    add code to remove event listener on Button

--- gift-list/src/components/Button.jsx
+++ gift-list/src/components/Button.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../utils/css/Button.css';
 
 const Button = ({ type, value, handleClick }) => {
+	const refButton = useRef();
+
+	useEffect(() => {
+		const reference = refButton.current;
+		if (reference) {
+			reference.addEventListener('click', handleClick);
+
+			return () => {
+				reference.removeEventListener('click', handleClick);
+			};
+		}
+	}, [handleClick]);
+
 	return (
-		<button className='button' type={type} onClick={handleClick}>
+		<button className='button' ref={refButton} type={type}>
 			{value}
 		</button>
 	);
